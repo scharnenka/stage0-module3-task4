9c2b8638f89633659a4a912cc4833ca107efd679
Stas <chernenkosa@gmail.com>
2022-11-24 02:39:22 +0200

    task solution

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char charC = 99;
+        char charH = 104;
+        char charA = 97;
+        char charR = 114;
     }
 }
